3b368ad4db4c21074ac7aa5b3df1d9d1fef38027
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2023-12-21 18:35:51 +0300

    Add authentication propoties

---
+++ WebProject/WebProject/Data/Migrations/20231221152957_auth1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebProject.Data.Migrations
+{
+    public partial class auth1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UFirstLastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UFirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Uusername",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UFirstLastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UFirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Uusername",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ WebProject/WebProject/Models/UserDetail.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebProject.Models
+{
+    public class UserDetail : IdentityUser
+    {
+
+        public string Uusername { get; set; }
+
+        public string UFirstName { get; set; }
+
+        public string UFirstLastName { get; set; }
+
+
+
+    }
+}
