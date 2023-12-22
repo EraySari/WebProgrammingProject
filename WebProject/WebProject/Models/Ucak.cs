df2a03e38e12d5fcb26dd4293939cb6627b65070
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2023-12-22 15:12:08 +0300

    Add Validations properites

--- WebProject/WebProject/Models/Ucak.cs
+++ WebProject/WebProject/Models/Ucak.cs
@@ -1,19 +1,29 @@
-﻿namespace WebProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebProject.Models
 {
     public class Ucak
     {
         public int UcakID { get; set; }
 
+        [Required(ErrorMessage ="Kalkış Noktası Giriniz!")]
         public string Departure { get; set; }
 
+        [Required(ErrorMessage = "Varış Noktası Giriniz!")]
         public string Arrival { get; set; }
 
+        [Required(ErrorMessage = "Uçak Modelini Giriniz!")]
         public string PlaneModel { get; set; }
 
+        [Required(ErrorMessage = "Bilet Ücretini Giriniz!")]
+        [Range(300, 1200, ErrorMessage ="Bilet Fiyat Aralığı 300 - 1200 Arasında Olmalıdır!")]
         public int Price { get; set; }
 
+        [Required(ErrorMessage = "Koltuk Sayısını Giriniz!")]
+        [Range(30, 90, ErrorMessage = "Koltuk Sayısı 30 - 90 Aralığında Olmalıdır! ")]
         public int Seat { get; set; }
 
+        [Required(ErrorMessage = "Kalkış Zamanını Giriniz!")]
         public DateTime DateTime { get; set; }
 
 
--- WebProject/WebProject/Models/User.cs
+++ WebProject/WebProject/Models/User.cs
@@ -1,20 +1,32 @@
-﻿namespace WebProject.Models
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebProject.Models
 {
     public class User
     {
 
         public int UserID { get; set; }
 
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "User Name Giriniz!")]
         public string UserName { get; set; }
 
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage ="First Name Giriniz!")]
         public string FirstName { get; set; }
 
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Last Name Giriniz!")]
         public string LastName { get; set; }
 
+        [System.ComponentModel.DataAnnotations.Required]
+        [EmailAddress(ErrorMessage ="Düzgün Email Adresi Giriniz!")]
         public string Email { get; set; }
 
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Koltuk Numarası Giriniz!")]
+        [Range(0,90,ErrorMessage ="Koltuk Numarası 0-90 Arasında Olmalı!")]
         public int KoltukNo { get; set; }
 
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Parola Giriniz!")]
         public string Password { get; set; }
 
         public int UcakID { get; set; }
